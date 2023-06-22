6bf57d93ca2472f7b481aea91cd7fe321211f888
junioralcant <junioralcant111@gmail.com>
2023-06-21 22:14:28 -0300

    feat: create Text component

---
+++ src/components/Text/Text.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import {
+  Text as RnText,
+  TextProps as RnTextProps,
+  TextStyle,
+} from 'react-native';
+
+type TextProps = {
+  preset?: TextVariants;
+} & RnTextProps;
+
+export function Text({
+  children,
+  preset = 'headingMedium',
+  style,
+  ...rest
+}: TextProps) {
+  return (
+    <RnText style={[$fontSizes[preset], style]} {...rest}>
+      {children}
+    </RnText>
+  );
+}
+
+type TextVariants =
+  | 'headingLarge'
+  | 'headingMedium'
+  | 'headingSmall'
+  | 'paragraphLarge'
+  | 'paragraphMedium'
+  | 'paragraphSmall'
+  | 'paragraphCaption'
+  | 'paragraphCaptionSmall';
+
+const $fontSizes: Record<TextVariants, TextStyle> = {
+  headingLarge: {fontSize: 32, lineHeight: 38.4},
+  headingMedium: {fontSize: 22, lineHeight: 26.4},
+  headingSmall: {fontSize: 18, lineHeight: 23.4},
+
+  paragraphLarge: {fontSize: 18, lineHeight: 25.2},
+  paragraphMedium: {fontSize: 16, lineHeight: 22.4},
+  paragraphSmall: {fontSize: 14, lineHeight: 19.6},
+
+  paragraphCaption: {fontSize: 12, lineHeight: 16.8},
+  paragraphCaptionSmall: {fontSize: 10, lineHeight: 14},
+};
