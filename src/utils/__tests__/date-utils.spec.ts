abcb6f8d7bc92c59b70d12545ad4563cdd09f6ab
junioralcant <junioralcant111@gmail.com>
2023-12-16 09:27:10 -0300

    feat: ensure capitalizeFirstLetter remove leading/trailing spaces

--- src/utils/__tests__/date-utils.spec.ts
+++ src/utils/__tests__/date-utils.spec.ts
@@ -1,6 +1,14 @@
 import {stringUtils} from '@utils';
 
-test('capitalizeFirstLetter', () => {
-  const name = stringUtils.capitalizeFirstLetter('ana maria');
-  expect(name).toBe('Ana Maria');
+describe('stringUtils', () => {
+  describe('capitalizeFirstLetter', () => {
+    it('should capitalize the first letter fo each word', () => {
+      const name = stringUtils.capitalizeFirstLetter('ana maria');
+      expect(name).toBe('Ana Maria');
+    });
+
+    it('should remove leading/trailing spaces', () => {
+      expect(stringUtils.capitalizeFirstLetter(' ana maria')).toBe('Ana Maria');
+    });
+  });
 });
